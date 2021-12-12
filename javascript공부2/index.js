4113c9e265eef799d5720e0beb11b9f95e36a460
Creative-Lee <77152650+Creative-Lee@users.noreply.github.com>
2021-12-12 21:41:27 +0900

    21.12.12
    Deep copy  포스팅

--- javascript공부2/index.js
+++ javascript공부2/index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 let sum = 0;
 let salaries = {
   John: 100,
@@ -8,3 +9,22 @@ let salaries = {
 for(let key in salaries){
 	sum += salaries[key]
 }
+=======
+let info = {
+  boy : {
+    isGood : true,
+    isPretty : false,
+  },
+  girl : {
+    isGood : true,
+    isPretty : true,
+  }
+}
+
+let clone = {...info}
+
+clone.boy.isGood = false
+
+console.log(info.boy.isGood)  //false
+console.log(clone.boy.isGood)  //false
+>>>>>>> Stashed changes
