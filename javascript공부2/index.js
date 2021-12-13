a0426cee3285eb13f9c739a0a559f639ff49e544
Creative-Lee <77152650+Creative-Lee@users.noreply.github.com>
2021-12-13 21:51:54 +0900

    21.12.13
    parcel 알아보기

--- javascript공부2/index.js
+++ javascript공부2/index.js
@@ -1,30 +1,15 @@
-<<<<<<< Updated upstream
-let sum = 0;
-let salaries = {
-  John: 100,
-  Ann: 160,
-  Pete: 130
-}
+import _ from 'lodash'
 
-for(let key in salaries){
-	sum += salaries[key]
-}
-=======
 let info = {
   boy : {
     isGood : true,
     isPretty : false,
-  },
-  girl : {
-    isGood : true,
-    isPretty : true,
   }
 }
 
-let clone = {...info}
+let clone = _.cloneDeep(info);
 
 clone.boy.isGood = false
 
-console.log(info.boy.isGood)  //false
-console.log(clone.boy.isGood)  //false
->>>>>>> Stashed changes
+console.log(info.boy.isGood)
+console.log(clone.boy.isGood)
