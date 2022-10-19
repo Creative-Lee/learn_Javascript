f1c9e94720ecb67b16441a5d693c2276c3ce7a71
Dohyeon Lee <dlehgus1110@naver.com>
2022-10-19 11:36:02 +0900

    feat: 메뉴 이름 수정 기능 추가

--- moonbucks/src/js/index.js
+++ moonbucks/src/js/index.js
@@ -5,33 +5,6 @@ class App {
 	constructor() {
 		this.initEvent()
 	}
-	addMenuName() {
-		const menuName = $('#menu-name').value
-		if (!menuName) {
-			alert('입력 없음')
-			return
-		}
-		const getMenuItemTemplit = menuName => {
-			return `<li class="menu-list-item d-flex items-center py-2">
-			<span class="w-100 pl-2 menu-name">${menuName}</span>
-			<button
-				type="button"
-				class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-			>
-				수정
-			</button>
-			<button
-				type="button"
-				class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-			>
-				삭제
-			</button>
-		</li>`
-		}
-		$('#menu-list').insertAdjacentHTML('beforeend', getMenuItemTemplit(menuName))
-		$('#menu-name').value = ''
-	}
-
 	initEvent() {
 		$('#menu-form').addEventListener('submit', e => {
 			e.preventDefault()
@@ -46,6 +19,48 @@ class App {
 				this.addMenuName()
 			}
 		})
+
+		$('#menu-list').addEventListener('click', ({ target }) => {
+			if (target.classList.contains('menu-edit-button')) {
+				this.updateMenuName(target)
+				return
+			}
+		})
+	}
+
+	getMenuItemTemplit(menuName) {
+		return `<li class="menu-list-item d-flex items-center py-2">
+		<span class="w-100 pl-2 menu-name">${menuName}</span>
+		<button
+			type="button"
+			class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+		>
+			수정
+		</button>
+		<button
+			type="button"
+			class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+		>
+			삭제
+		</button>
+	</li>`
+	}
+	addMenuName() {
+		const menuName = $('#menu-name').value
+		if (!menuName) {
+			alert('입력 없음')
+			return
+		}
+
+		$('#menu-list').insertAdjacentHTML('beforeend', this.getMenuItemTemplit(menuName))
+		$('#menu-name').value = ''
+	}
+	updateMenuName(target) {
+		const $menuName = target.closest('li').querySelector('.menu-name')
+		const updatedMenuName = prompt('수정할 메뉴 이름을 입력하세요', $menuName.innerText)
+		if (updatedMenuName) {
+			$menuName.innerText = updatedMenuName
+		}
 	}
 }
 
