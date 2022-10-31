d923048f0e21e1e6e3a0081a290a810701b4c681
Dohyeon Lee <dlehgus1110@naver.com>
2022-10-31 09:51:36 +0900

    refactor: event 초기화 메서드 controller로 분리

--- moonbucks/src/js/controllers/CafeMenuController.js
+++ moonbucks/src/js/controllers/CafeMenuController.js
@@ -9,6 +9,37 @@ export default class CafeMenuController {
 		}
 		this.currentMenuCategory = 'espresso'
 	}
+
+	initEventListeners() {
+		$('#menu-form').addEventListener('submit', e => {
+			e.preventDefault()
+		})
+
+		$('#menu-submit-button').addEventListener('click', () => this.addMenuName())
+
+		$('#menu-name').addEventListener('keypress', ({ key }) => {
+			if (key !== 'Enter') return
+			this.addMenuName()
+		})
+
+		$('#menu-list').addEventListener('click', ({ target }) => {
+			if (target.classList.contains('menu-sold-out-button')) {
+				this.toggleSoldout(target)
+				return
+			}
+			if (target.classList.contains('menu-edit-button')) {
+				this.updateMenuName(target)
+				return
+			}
+			if (target.classList.contains('menu-remove-button')) {
+				this.removeMenuName(target)
+			}
+		})
+
+		$('.nav-menu').addEventListener('click', ({ target }) => {
+			this.changeCategory(target)
+		})
+	}
 	addMenuName() {
 		const menuName = $('#menu-name').value
 		if (!menuName) {
--- moonbucks/src/js/index.js
+++ moonbucks/src/js/index.js
@@ -3,14 +3,6 @@ import store from './store/index.js'
 
 class App {
 	constructor() {
-		this.cafeMenu = {
-			espresso: [],
-			frappuccino: [],
-			blended: [],
-			teavana: [],
-			desert: [],
-		}
-		this.currentMenuCategory = 'espresso'
 		this.init()
 	}
 
@@ -21,36 +13,6 @@ class App {
 		}
 		this.render()
 		this.initEventListeners()
-	}
-	initEventListeners() {
-		$('#menu-form').addEventListener('submit', e => {
-			e.preventDefault()
-		})
-
-		$('#menu-submit-button').addEventListener('click', () => this.addMenuName())
-
-		$('#menu-name').addEventListener('keypress', ({ key }) => {
-			if (key !== 'Enter') return
-			this.addMenuName()
-		})
-
-		$('#menu-list').addEventListener('click', ({ target }) => {
-			if (target.classList.contains('menu-sold-out-button')) {
-				this.toggleSoldout(target)
-				return
-			}
-			if (target.classList.contains('menu-edit-button')) {
-				this.updateMenuName(target)
-				return
-			}
-			if (target.classList.contains('menu-remove-button')) {
-				this.removeMenuName(target)
-			}
-		})
-
-		$('.nav-menu').addEventListener('click', ({ target }) => {
-			this.changeCategory(target)
-		})
 	}
 }
 
